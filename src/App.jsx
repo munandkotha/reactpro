f7bf1b62dd2d0616de08eb694f5c7e9f007788a9
munandkotha <munandreddy@gmail.com>
2024-07-16 00:53:29 -0400

    Created React App

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Hello from "./Components/Hello"
+
+function App() {
+  return <Hello />
+}
+export default App
---
+++ src/Components/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello() {
+    return (
+        <h1>Hello Srilaxmi!</h1>
+    )
+}
+
+export default Hello;
